a1b7d2d5a242c6e442a69f456c5590f8e7b4d978
sireesha tammana <siri0981t@gmail.com>
2021-05-24 18:26:24 -0400

    forkCommit

---
+++ ForkFinalPractice/src/gitFork/extractIntegerFromString.java
@@ -0,0 +1,24 @@
+package gitFork;
+
+public class extractIntegerFromString{
+
+		public static void main(String[] args) {
+		
+	   String str= "x12y45z67";
+	  for(char ch:str.toCharArray()) {
+		  
+	 if(Character.isDigit(ch)){
+		System.out.println(ch +""); 
+			  
+			  
+		  }
+		  
+		  
+	  }
+	  
+	   
+		}
+
+	}
+
+
